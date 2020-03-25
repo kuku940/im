3bf55a4ba2d700021dba0b44f4156a7c7388a49a
章小雨 <roingeek@qq.com>
2020-03-25 23:26:25 +0800

    im init commit

---
+++ src/main/java/cn/xiaoyu/learning/im/common/ThreadPoolManager.java
@@ -0,0 +1,39 @@
+package cn.xiaoyu.learning.im.common;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 线程池管理器
+ *
+ * @author Roin zhang
+ */
+
+public class ThreadPoolManager {
+    private static final int CORE_POOL_SIZE = 20;
+    private static final int MAXIMUM_POOL_SIZE = 30;
+    private static ExecutorService instance;
+
+    public static ExecutorService getInstance() {
+        ExecutorService localInstance = instance;
+        if (localInstance == null) {
+            synchronized (ThreadPoolManager.class) {
+                localInstance = instance;
+                if (localInstance == null) {
+                    localInstance = instance = new ThreadPoolExecutor(CORE_POOL_SIZE,
+                            MAXIMUM_POOL_SIZE,
+                            30000L,
+                            TimeUnit.MILLISECONDS,
+                            new LinkedBlockingQueue<>(),
+                            // google guava线程池命名工具
+                            new ThreadFactoryBuilder().setNameFormat("learning-%d").build());
+                }
+            }
+        }
+        return localInstance;
+    }
+}
---
+++ src/main/java/cn/xiaoyu/learning/im/protocol/request/LoginRequestPacket.java
@@ -0,0 +1,20 @@
+package cn.xiaoyu.learning.im.protocol.request;
+
+import cn.xiaoyu.learning.im.protocol.Packet;
+import cn.xiaoyu.learning.im.protocol.command.Command;
+import lombok.Data;
+
+/**
+ * @author roin.zhang
+ * @date 2019/9/24
+ */
+@Data
+public class LoginRequestPacket extends Packet {
+    private String username;
+    private String password;
+
+    @Override
+    public Byte getCommand() {
+        return Command.LOGIN_REQUEST;
+    }
+}
